70fc34f14b92c7d9860f18f933c4c494a10841ee
allen3_huang <allen3_huang@asus.com>
2025-04-05 15:39:45 +0800

    feat: antd的基本使用

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "antd";
+
 function Home() {
-  return <div>我是home 組件</div>;
+  return (
+    <div>
+      我是home組件
+      <Button type="primary">我是一個按鈕</Button>
+    </div>
+  );
 }
 
 export default Home;
