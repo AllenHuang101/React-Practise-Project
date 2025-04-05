bc1200c10eead3b0458171b53ef345a48a74be8e
allen3_huang <allen3_huang@asus.com>
2025-04-05 16:03:07 +0800

    feat: 自己封裝一個 antd 組件

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,10 +1,26 @@
 import { Button } from "antd";
+import { FC } from "react";
+
+interface ButtonProps {
+  type?: string;
+  children: React.ReactNode;
+}
+
+const XuButton: FC<ButtonProps> = (props) => {
+  const cssStyle = {
+    padding: "10px 20px",
+    borderRadius: "10px",
+    backgroundColor: props.type === "primary" ? "blue" : "",
+  };
+  return <button style={cssStyle}>{props.children}</button>;
+};
 
 function Home() {
   return (
     <div>
       我是home組件
       <Button type="primary">我是一個按鈕</Button>
+      <XuButton>我是一個自定義按鈕</XuButton>
     </div>
   );
 }
