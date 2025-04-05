1109386ac522f5b0fd14b08667ca1beb8973efdc
allen3_huang <allen3_huang@asus.com>
2025-04-05 17:05:55 +0800

    feat: 登入頁面靜態結構

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,10 +1,47 @@
+import { Button, Form, Input } from "antd";
+import bg from "../../assets/bg.jpg";
+import lgbg from "../../assets/lgbg.jpg";
+import logo from "../../assets/logo.png";
+
 import "./index.scss";
 
 function Login() {
   return (
-    <div className="login">
-      我是登入組件
-      <p className="aa">Hello</p>
+    <div className="login" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="lgbg" style={{ backgroundImage: `url(${lgbg})` }}>
+        <div className="part">
+          <div className="title">
+            <div className="logo">
+              <img src={logo} width={100} />
+            </div>
+            <h1>朋遠智慧園區管理平台</h1>
+          </div>
+          <Form>
+            <Form.Item
+              label="用戶名"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="密碼"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item>
+              <Button>Submit</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
