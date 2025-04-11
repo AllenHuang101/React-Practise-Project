36645791933c3b080ee4f66a3d164ef834f02e1a
allen3_huang <allen3_huang@asus.com>
2025-04-11 15:49:43 +0800

    feat: 權限控制之遞歸處理路由數據

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { generateRoutes } from "./utils/generatesRoutes";
 
 function App() {
+  const { menuList } = useSelector((state: any) => state.authSlice);
+
+  useEffect(() => {
+    const routers = generateRoutes(menuList);
+    console.log("routers", routers);
+    console.log("menuList", menuList);
+  }, [menuList]);
+
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
